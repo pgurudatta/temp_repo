244905b6a62a80324b457b89bccc03290ec2746d
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-07-04 12:30:00 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,39 +1,74 @@
 /*
-Sample code for vulnerable type: Use of Insufficiently Random Values
-CWE : CWE-330
-Description : Use of Insufficiently Random Values
+Sample code for vulnerable type: Hardcoded Secret
+CWE : CWE-547
+Description : Use of Hard-coded, Security-relevant Constants
 */
 package main
 
 import (
-	"math/rand"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"log"
 )
 
-var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-// Function to generate a random password
-func generatePassword() string {
-	s := make([]rune, 20)
-	for i := range s {
-		s[i] = getRandomChar()
-	}
-	return string(s)
-}
-
-// Function to get a random character from the charset
-func getRandomChar() rune {
-	return charset[rand.Intn(len(charset))] // Source
-}
-
-// Function to print the generated password
-func printPassword(password string) {
-	println(password) // Sink
-}
-
 func main() {
-	// Generate a random password
-	password := generatePassword()
+	plaintext := "Sensitive data to encrypt"
+	key := []byte("myweaksecretkey") // source (Inadequate key length)
 
-	// Print the generated password
-	printPassword(password)
+	// Encrypt the plaintext using AES encryption with CBC mode
+	ciphertext, err := encryptAES(plaintext, key)
+	if err != nil {
+		log.Fatalf("Encryption error: %v", err)
+	}
+
+	fmt.Printf("Encrypted text: %s\n", ciphertext)
+
+	// Decrypt the ciphertext (for demonstration purposes)
+	decryptedText, err := decryptAES(ciphertext, key)
+	if err != nil {
+		log.Fatalf("Decryption error: %v", err)
+	}
+
+	fmt.Printf("Decrypted text: %s\n", decryptedText)
+}
+
+func encryptAES(plaintext string, key []byte) (string, error) {
+	block, err := aes.NewCipher(key) //sink
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plaintext))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := rand.Read(iv); err != nil {
+		return "", err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], []byte(plaintext))
+
+	return base64.URLEncoding.EncodeToString(cipherText), nil
+}
+
+func decryptAES(ciphertext string, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText, err := base64.URLEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(cipherText, cipherText)
+
+	return string(cipherText), nil
 }
