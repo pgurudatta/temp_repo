d0d10879f2ace62d44cce5976e1228a50a4e3450
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-07-18 15:22:10 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,38 +1,16 @@
 /*
-Sample code for vulnerable type: Sensitive Cookie Without 'HttpOnly' Flag
-CWE : CWE-1004
-Description : Sensitive Cookie Without 'HttpOnly' Flag
+Sample code for vulnerable type: Use of Password Hash With Insufficient Computational Effort
+CWE : CWE-916
+Description : Use of Password Hash With Insufficient Computational Effort
 */
 package main
-
 import (
+	"crypto/md5"
 	"fmt"
-	"net/http"
-	"time"
 )
-
 func main() {
-	http.HandleFunc("/login", handleLogin)
-	http.ListenAndServe(":8080", nil)
-}
-
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-	// Simulated login functionality (authenticate user)
-	authenticated := true
-
-	if authenticated {
-		// Set a sensitive cookie without the "HttpOnly" flag
-		cookie := http.Cookie{
-			Name:     "sessionID",
-			Value:    "exampleSessionToken",
-			Expires:  time.Now().Add(24 * time.Hour), // Expires in 24 hours
-			Path:     "/",
-			HttpOnly: false, // Vulnerable: Cookie accessible to client-side JavaScript
-		}
-		http.SetCookie(w, &cookie)  //source and sink
-
-		fmt.Fprintf(w, "Login successful. Session cookie set.")
-	} else {
-		http.Error(w, "Authentication failed", http.StatusUnauthorized)
-	}
+	data := "SensitiveData123"
+	hash := md5.Sum([]byte(data)) //source and sink
+	hashString := fmt.Sprintf("%x", hash)
+	fmt.Printf("MD5 Hash: %s\n", hashString)
 }
