1c9e38fb257a8f7830a1077fa0cba2bc8f38164d
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-07-18 15:33:51 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,16 +1,15 @@
-/*
-Sample code for vulnerable type: Use of Password Hash With Insufficient Computational Effort
-CWE : CWE-916
-Description : Use of Password Hash With Insufficient Computational Effort
-*/
 package main
+
 import (
-	"crypto/md5"
-	"fmt"
+    "crypto/md5"
+    "fmt"
 )
+
 func main() {
-	data := "SensitiveData123"
-	hash := md5.Sum([]byte(data)) //source and sink
-	hashString := fmt.Sprintf("%x", hash)
-	fmt.Printf("MD5 Hash: %s\n", hashString)
+    data := "Hello, world!"
+
+    // Using MD5 (which is considered broken for cryptographic purposes)
+    hash := md5.Sum([]byte(data))
+    
+    fmt.Printf("MD5 hash of '%s': %x\n", data, hash)
 }
