248a0e1e13066adcbb7d439e56b122c181583e88
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-07-18 15:38:10 +0530

    Update test.go

--- test.go
+++ test.go
@@ -6,10 +6,30 @@ import (
 )
 
 func main() {
-    data := "Hello, world!"
+    password := "mysecretpassword"
 
-    // Using MD5 (which is considered broken for cryptographic purposes)
-    hash := md5.Sum([]byte(data))
-    
-    fmt.Printf("MD5 hash of '%s': %x\n", data, hash)
+    // Simulating storage of hashed password
+    hashedPassword := hashPassword(password)
+
+    // Simulating login attempt
+    loginAttempt := "mysecretpassword"
+
+    // Verify login
+    if checkPassword(loginAttempt, hashedPassword) {
+        fmt.Println("Login successful!")
+    } else {
+        fmt.Println("Login failed!")
+    }
+}
+
+// Function to hash the password using MD5 (vulnerable)
+func hashPassword(password string) string {
+    hash := md5.Sum([]byte(password))
+    return fmt.Sprintf("%x", hash)
+}
+
+// Function to check if the password matches the hashed value
+func checkPassword(password, hashedPassword string) bool {
+    // Recompute the hash of the provided password and compare
+    return hashedPassword == hashPassword(password)
 }
