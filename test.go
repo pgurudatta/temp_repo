064d192fab5b21e18e0c3d66c8b6b9d9e6d71d7b
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-08-09 17:47:45 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
-import "io"
-import "crypto/md5"
-import "crypto/sha256"
-import "golang.org/x/crypto/blake2s"
+import (
+	"log"
+	"net/http"
+)
 
-func main () {
-        h_md5 := md5.New()
-        h_sha := sha256.New()
-        h_blake2s, _ := blake2s.New256(nil)
-        io.WriteString(h_md5, "Go Language Secure Coding Practices")
-        io.WriteString(h_sha, "Go Language Secure Coding Practices")
-        io.WriteString(h_blake2s, "Welcome to Go Language Secure Coding Practices")
-        fmt.Printf("MD5        : %x\n", h_md5.Sum(nil))
-        fmt.Printf("SHA256     : %x\n", h_sha.Sum(nil))
-        fmt.Printf("Blake2s-256: %x\n", h_blake2s.Sum(nil))
+func serve() {
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		user := r.Form.Get("user")
+		pw := r.Form.Get("password")
+
+		log.Printf("Registering new user %s with password %s.\n", user, pw)
+	})
+	http.ListenAndServe(":80", nil)
 }
